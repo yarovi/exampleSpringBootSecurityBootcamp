e0739ba0d8b541ee98e35ff82a2fb34419675997
Yasmani Rosado <tas185@gmail.com>
2025-09-07 08:49:33 -0500

    first commit

---
+++ src/main/java/org/yasmani/io/examplesecuritysprinbbootv1/ExampleSecuritySprinbBootv1Application.java
@@ -0,0 +1,13 @@
+package org.yasmani.io.examplesecuritysprinbbootv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSecuritySprinbBootv1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleSecuritySprinbBootv1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/yasmani/io/examplesecuritysprinbbootv1/ExampleSecuritySprinbBootv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yasmani.io.examplesecuritysprinbbootv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSecuritySprinbBootv1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
